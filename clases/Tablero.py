318ca7612b63b8c064ef91cdc8196db26195d156
BobiStudio <mgonzgarc@myuax.com>
2023-03-18 13:46:33 +0100

    cambios ruben

---
+++ clases/Tablero.py
@@ -0,0 +1,57 @@
+from itertools import product
+from clases import Conventions
+from clases import Case
+from clases.Barco import generar_barcos
+from clases.Case import generar_casillas
+
+  # performance / legibilidad:
+  num_lineas = Conventions.tablero_num_lineas
+  num_columnas = Conventions.tablero_num_columnas
+  num2l = Conventions.generar_num_linea
+  num2c = Conventions.generar_num_columna
+
+@staticmethod
+def __init__(self):
+# Creamos las casillas:
+  generar_casillas()
+# Creamos los barcos:
+  generar_barcos()
+ # Creamos la herramienta para poder seguir la situación
+  self.casillas_jugadas = set()
+  self.casillas_tocadas = set()
+  self.casillas_hundidas = set()
+  self.casillas_agua = set()
+  self.casillas_ocupadas = set()
+  self.casillas_libres = set()
+  self.casillas_libres.update(Case.instances.values())
+
+  # Generamos aquí los etiquetas para facilitar la visualización
+  self.etiqueta_lineas = [num2l(x) for x in range(num_lineas)]
+  self.etiqueta_columnas = [num2c(x) for x in range(num_columnas)]
+  trazo_horizontal = " --" + "+---" * 10 + "+"
+
+def ver(self):
+  print("   |", " | ".join(self.etiqueta_columnas), "|")
+  
+  iter_etiqueta_lineas = iter(self.etiqueta_lineas)
+  
+  for x, y in product(range(Conventions.tablero_num_lineas),
+                      range(Conventions.tablero_num_columnas)):
+  
+      # Trazo horizontal para cada nueva línea
+      if y == 0:
+          print(self.trazo_horizontal)
+          print(" {}".format(next(iter_etiqueta_lineas)), end="")
+  
+      casilla = Case.instances[x, y]
+      print(" |", casilla, end="")
+  
+      # Ver la barra vertical derecha de la tabla:
+      if y == 9:
+          print(" |")
+  # Ver la última línea horizontal
+  print(self.trazo_horizontal + "\n\n")
+
+
+
+
---
+++ juego.py
@@ -0,0 +1,78 @@
+import sys
+
+from clases import Barco
+from clases import Case
+from clases import Tablero
+
+
+
+
+from introducir import (
+    solicitar_introducir_si_o_no,
+    solicitar_introducir_casilla,
+)
+
+
+LONGITUDES_BARCOS = [2, 3, 3, 4, 4, 5]
+ORDINAL = 0x2680
+
+CASO_NO_JUGADO = chr(0x2610)
+CASO_TOCADO = chr(0x2611)
+CASO_AGUA = chr(0x2612)
+
+HORIZONTAL = 0
+VERTICAL = 1
+
+ORIENTACIONES = (VERTICAL, HORIZONTAL)
+
+def probar_fin_juego(self):
+    """Permite probar si el juego ha terminado o no"""
+    if len(Barco.casillas_ocupadas - self.casillas_jugadas) == 0:
+        print("Bravo. El juego ha terminado !")
+        return True
+    else:
+        return False
+    
+def jugar_tirada(self):
+        """Permite gestionar el dato introducido de una tirada"""
+        while True:
+            nombre_casilla = solicitar_introducir_casilla(
+                "Seleccionar una casilla (letra + cifra)")
+            # Encontrar la casilla a partir de su nombre
+            casilla = Case.instances[nombre_casilla]
+            # Probar si la casilla ya ha sido jugada
+            if casilla.jugada:
+                print("Esta casilla ya ha sido jugada, elija otra",
+                    file=sys.stderr)
+            else:
+                casilla.jugar()
+                break
+
+def jugar_una_partida():
+    """Algoritmo de una partida"""
+    # Creamos un tablero de juego vacío
+
+    tablero = Tablero()
+
+
+    while True:
+        tablero.ver()
+
+        tablero.jugar_tirada()
+
+        if tablero.probar_fin_juego():
+            # Si el juego ha terminado, salimos de la función
+            tablero.ver()
+            return
+
+def elegir_jugarOtra():
+    return solicitar_introducir_si_o_no(
+        "¿Desea volver a jugar? ? [s/n]")
+
+
+def jugar():
+    while True:
+        jugar_una_partida()
+
+        if not elegir_jugarOtra():
+            return
